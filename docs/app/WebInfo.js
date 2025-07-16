6821ef1f78f30bd47ebc0fbe6ffc15ba3d1b4a75
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-07-16 13:29:34 +0530

    Add WebInfo.js with metadata for Julie library

---
+++ docs/app/WebInfo.js
@@ -0,0 +1,4 @@
+const webInfo = {
+    title: "Julie: Build beautiful user interfaces.",
+    description: "Julie is a component library and design system designed to empower the creation of beautiful user interfaces across multiple platforms. ",
+}
