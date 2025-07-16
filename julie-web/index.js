d5fdb5f5eb1a5e0e90d144b5010a7c122e251fbd
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-07-16 16:14:33 +0530

    Add index.js: export components and styles for modular imports

---
+++ julie-web/index.js
@@ -0,0 +1,10 @@
+import "./styles/Type.css";
+import "./styles/Spacing.css";
+import "./styles/Color.css";
+
+export { default as Header } from "./header/Header";
+export { default as HeaderTitle } from "./header/HeaderTitle";
+export { default as HeaderLogoAndTitle } from "./header/HeaderLogoAndTitle";
+export { default as HeaderNavigation } from "./header/HeaderNavigation";
+
+export { default as Spacer } from "./spacer/Spacer";
