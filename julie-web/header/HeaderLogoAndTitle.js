241f36859715acac6212a53cf531b2335ec83c68
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-07-16 15:39:53 +0530

    Update HeaderLogoAndTitle.js: add "with-logo" class to header link

--- julie-web/header/HeaderLogoAndTitle.js
+++ julie-web/header/HeaderLogoAndTitle.js
@@ -3,7 +3,7 @@ import {Spacer} from "../index";
 
 const HeaderLogoAndTitle = ({logo, title}) => {
     return (
-        <Link href="/" className="header-link">
+        <Link href="/" className="header-link with-logo">
             <img className="header-logo" src={logo} alt="Header logo"/>
             <Spacer className="spacer-8px-width"></Spacer>
             <h1 className="title-medium semi-bold header-title">{title}</h1>
