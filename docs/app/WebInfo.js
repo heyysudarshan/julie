22d119bf0f7ed27557fca8ca2a3a530a5adb3aa8
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-07-16 13:29:51 +0530

    Export WebInfo object for better reusability in other modules

--- docs/app/WebInfo.js
+++ docs/app/WebInfo.js
@@ -2,3 +2,5 @@ const webInfo = {
     title: "Julie: Build beautiful user interfaces.",
     description: "Julie is a component library and design system designed to empower the creation of beautiful user interfaces across multiple platforms. ",
 }
+
+export default webInfo;
