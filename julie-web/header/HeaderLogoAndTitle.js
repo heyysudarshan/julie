76569c8d45181b3522e8a39529e90134da8ce767
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-07-16 15:00:47 +0530

    Add HeaderLogoAndTitle.js: component for logo and title linking to home page

---
+++ julie-web/header/HeaderLogoAndTitle.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+import {Spacer} from "../index";
+
+const HeaderLogoAndTitle = ({logo, title}) => {
+    return (
+        <Link href="/" className="header-link">
+            <img className="header-logo" src={logo} alt="Header logo"/>
+            <Spacer className="spacer-8px-width"></Spacer>
+            <h1 className="title-medium semi-bold header-title">{title}</h1>
+        </Link>
+    );
+};
+
+export default HeaderLogoAndTitle;
