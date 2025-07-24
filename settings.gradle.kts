adae92adcf5fd289df389b4ee8d5fb5fc2b9c6eb
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-07-24 10:41:55 +0530

    Set up Gradle build system with wrapper, properties, and configuration files

---
+++ settings.gradle.kts
@@ -0,0 +1,34 @@
+rootProject.name = "Julie"
+
+@Suppress("UnstableApiUsage")
+pluginManagement {
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+
+@Suppress("UnstableApiUsage")
+dependencyResolutionManagement {
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+    }
+}
