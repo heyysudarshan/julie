69713820b69edc58155e6ff27d5cf80508c28d21
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-07-24 11:06:22 +0530

    Set custom build directory in root build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,3 +2,6 @@ plugins {
     alias(libs.plugins.kotlin.multiplatform) apply false
     alias(libs.plugins.android.library) apply false
 }
+
+// Custom build directory
+layout.buildDirectory.set(file(path = "$rootDir/.build/project"))
