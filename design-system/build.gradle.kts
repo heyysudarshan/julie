7b27bfe907dac33aac5ed0ed23ae6ada0279143e
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-07-24 10:54:37 +0530

    Set up Kotlin Multiplatform targets for design-system module

--- design-system/build.gradle.kts
+++ design-system/build.gradle.kts
@@ -2,3 +2,13 @@ plugins {
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.android.library)
 }
+
+kotlin {
+    androidTarget()
+
+    jvm("desktop")
+
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
+}
