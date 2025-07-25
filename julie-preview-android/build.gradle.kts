deeeb52cfd9f557ee7fc1471f436227185dae5ac
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-07-25 23:12:48 +0530

    Set up `julie-preview-android` module build configuration

--- julie-preview-android/build.gradle.kts
+++ julie-preview-android/build.gradle.kts
@@ -1,3 +1,46 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.compose.compiler)
+}
+
+group = libs.versions.juliePreviewAndroid.group.get()
+version = libs.versions.juliePreviewAndroid.version.get()
+
+android {
+    namespace = libs.versions.juliePreviewAndroidApp.namespace.get()
+    compileSdk = libs.versions.juliePreviewAndroidApp.compileSdk.get().toInt()
+
+    defaultConfig {
+        applicationId = libs.versions.juliePreviewAndroidApp.applicationId.get()
+        targetSdk = libs.versions.juliePreviewAndroidApp.targetSdk.get().toInt()
+        minSdk = libs.versions.juliePreviewAndroidApp.minSdk.get().toInt()
+        versionCode = libs.versions.juliePreviewAndroidApp.versionCode.get().toInt()
+        versionName = libs.versions.juliePreviewAndroidApp.versionName.get()
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlin.compilerOptions {
+        jvmTarget = JvmTarget.JVM_11
+    }
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
 
 }
+
+// Custom build directory
+layout.buildDirectory.set(file(path = "$rootDir/.build/juliePreviewAndroid"))
