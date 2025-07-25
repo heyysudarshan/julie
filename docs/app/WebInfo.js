62bd9aac3449dc6cce728166e1fc1ee4688a1b81
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-07-25 12:05:58 +0530

    Add `WebInfo.js` to define the documentation's metadata (title and description)

---
+++ docs/app/WebInfo.js
@@ -0,0 +1,6 @@
+const WebInfo = {
+    title: "Julie Documentation",
+    description: "Julie is a component library and design system built to help you create beautiful and consistent apps across all platforms.",
+}
+
+export default WebInfo;
