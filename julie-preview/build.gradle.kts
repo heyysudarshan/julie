7969ca03b8e1a8270f1edbef39e6fff3d2f16719
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-07-25 18:45:17 +0530

    Configure julie-preview module for multiplatform and Android builds

--- julie-preview/build.gradle.kts
+++ julie-preview/build.gradle.kts
@@ -1,3 +1,40 @@
 plugins {
-
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.compose.multiplatform)
+    alias(libs.plugins.compose.compiler)
 }
+
+group = libs.versions.juliePreview.group.get()
+version = libs.versions.juliePreview.version.get()
+
+kotlin {
+    androidTarget()
+
+    jvm("desktop")
+
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64(),
+    ).forEach { iosTarget ->
+        iosTarget.binaries.framework {
+            baseName = "ComposeApp"
+            isStatic = true
+        }
+    }
+
+    sourceSets {
+        commonMain.dependencies {
+            implementation(libs.compose.multiplaform.ui)
+        }
+    }
+}
+
+android {
+    namespace = libs.versions.juliePreview.namespace.get()
+    compileSdk = libs.versions.juliePreview.compileSdk.get().toInt()
+}
+
+// Custom build directory
+layout.buildDirectory.set(file(path = "$rootDir/.build/juliePreview"))
