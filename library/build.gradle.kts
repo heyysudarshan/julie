91ffe58e3ce33894038b5b53dfd61456f4a1481a
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-07-30 16:30:49 +0530

    Configure Kotlin multiplatform targets and dependencies in `library` module.

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -1,5 +1,36 @@
 plugins {
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.compose.multiplatform)
+    alias(libs.plugins.compose.compiler)
+}
 
+group = libs.versions.library.group.get()
+version = libs.versions.library.version.get()
+
+kotlin {
+    androidTarget()
+
+    jvm("desktop")
+
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
+
+    sourceSets {
+        commonMain.dependencies {
+            implementation(libs.compose.multiplatform.ui)
+        }
+    }
+}
+
+android {
+    namespace = libs.versions.library.androidLibrary.namespace.get()
+    compileSdk = libs.versions.library.androidLibrary.compileSdk.get().toInt()
+
+    defaultConfig {
+        minSdk = libs.versions.library.androidLibrary.minSdk.get().toInt()
+    }
 }
 
 // Custom build directory
