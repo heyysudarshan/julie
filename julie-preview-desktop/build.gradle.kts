36309e3bd577aa00abe66a4e7a3ad96384edaed1
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-08-01 18:29:23 +0530

    Apply "org.jetbrains.compose.hot-reload" plugin to julie-preview-desktop module

--- julie-preview-desktop/build.gradle.kts
+++ julie-preview-desktop/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.kotlin.compiler)
     alias(libs.plugins.compose.multiplatform)
     alias(libs.plugins.compose.compiler)
+    alias(libs.plugins.compose.hot.reload)
 }
 
 group = libs.versions.juliePreviewDesktop.group.get()
