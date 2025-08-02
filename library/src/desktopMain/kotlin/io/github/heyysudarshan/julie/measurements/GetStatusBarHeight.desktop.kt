ea93c6ffb1221bac8bc431031dcc21d6855bd926
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-08-02 13:44:35 +0530

    Implement `getStatusBarHeight` for Desktop in `library` module

---
+++ library/src/desktopMain/kotlin/io/github/heyysudarshan/julie/measurements/GetStatusBarHeight.desktop.kt
@@ -0,0 +1,10 @@
+package io.github.heyysudarshan.julie.measurements
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+@Composable
+actual fun getStatusBarHeight(): Dp {
+    return 0.dp
+}
