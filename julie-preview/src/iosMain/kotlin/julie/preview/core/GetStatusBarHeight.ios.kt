6465e9ed6d0fd3a6ea1bd12779b017bde21cb855
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-08-02 13:34:38 +0530

    Implement `getStatusBarHeight` for iOS in `julie-preview` module

---
+++ julie-preview/src/iosMain/kotlin/julie/preview/core/GetStatusBarHeight.ios.kt
@@ -0,0 +1,12 @@
+package julie.preview.core
+
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.Dp
+
+@Composable
+internal actual fun getStatusBarHeight(): Dp {
+    return WindowInsets.statusBars.asPaddingValues().calculateTopPadding()
+}
