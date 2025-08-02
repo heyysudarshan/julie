14c1cbecf8809f5332ba7dc863c91eeb5da1359d
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-08-02 13:41:46 +0530

    Add `getStatusBarHeight` method declaration in `julie` module

---
+++ library/src/commonMain/kotlin/io/github/heyysudarshan/julie/measurements/GetStatusBarHeight.kt
@@ -0,0 +1,7 @@
+package io.github.heyysudarshan.julie.measurements
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.Dp
+
+@Composable
+internal expect fun getStatusBarHeight(): Dp
