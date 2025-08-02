e12a46938494089119bf9572a5748578a803b0fd
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-08-02 12:47:16 +0530

    Enable edge-to-edge display in `MainActivity` of `julie-preview-android` module

--- julie-preview-android/src/main/kotlin/julie/preview/android/MainActivity.kt
+++ julie-preview-android/src/main/kotlin/julie/preview/android/MainActivity.kt
@@ -3,11 +3,13 @@ package julie.preview.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         installSplashScreen()
         setContent {
 
