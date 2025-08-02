be4a3f7310bbe040148305cbc6f78a836b963e92
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-08-02 13:33:35 +0530

    Implement `getStatusBarHeight` for Desktop in `julie-preview` module

---
+++ julie-preview/src/desktopMain/kotlin/julie/preview/core/GetStatusBarHeight.desktop.kt
@@ -0,0 +1,10 @@
+package julie.preview.core
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+@Composable
+internal actual fun getStatusBarHeight(): Dp {
+    return 0.dp
+}
